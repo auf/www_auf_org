de18cf7d353bd2571fcdd297133c16f52d6e010f
Patrick Hetu <patrick.hetu@auf.org>
2015-03-12 14:34:54 -0400

    manage.py: ajustement pour sdk django 1.6

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 SITE_ROOT = os.path.dirname(__file__)
 
-sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django14_auf/'))
+sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django16_auf/'))
 sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django16_base/'))
 sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django16_auf_dev/'))
 
