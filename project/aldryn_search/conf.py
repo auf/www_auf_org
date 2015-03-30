75c8f388e72bc4aa308b793403c4da261ad0c4ef
Patrick Hetu <patrick.hetu@auf.org>
2015-03-30 10:07:36 -0400

    correction d'une exclusion trop large

---
+++ project/aldryn_search/conf.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+from appconf import AppConf
+
+
+class AldrynSearchAppConf(AppConf):
+
+    CMS_PAGE = True
+    DEFAULT_LANGUAGE = settings.LANGUAGE_CODE
+    INDEX_BASE_CLASS = 'aldryn_search.base.AldrynIndexBase'
+    LANGUAGE_FROM_ALIAS = 'aldryn_search.utils.language_from_alias'
+    PAGINATION = 10
+    REGISTER_APPHOOK = True
+
+    class Meta:
+        prefix = 'ALDRYN_SEARCH'
