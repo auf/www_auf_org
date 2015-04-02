a5391fce583ee4ee7df063654661389b543e47d8
Patrick Hetu <patrick.hetu@auf.org>
2015-04-02 14:35:00 -0400

    typo

--- project/offre_emploi/urls.py
+++ project/offre_emploi/urls.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 
 urlpatterns = patterns('offre_emploi.views',
