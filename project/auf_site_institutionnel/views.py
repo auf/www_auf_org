f51a331a9a5892620b729c487f24e768dd483f01
Patrick Hetu <patrick.hetu@auf.org>
2015-04-14 13:26:02 -0400

    correction de la liste des membres

--- project/auf_site_institutionnel/views.py
+++ project/auf_site_institutionnel/views.py
@@ -399,7 +399,7 @@ def membre(request, slugRegion=''):
     #if request.method == 'GET': # If the form has been submitted...
     item_list = MembreFilter(request.GET or None, queryset = Etablissement.objects.filter(**dictFilter))
 
-    item_list2 = item_list2.filter(status=3).order_by('-date_maj')
+    item_list = item_list.filter(status=3).order_by('-date_maj')
 
     return render_to_response('article.html', {'form': item_list.form, 'membre_list': item_list, 'page_title': 'Liste des membres'}, context_instance = RequestContext(request))
 
