f2f8e6e9c3549ff9dd61f42a8e4198d265e332b4
Eric Mc Sween <eric.mcsween@auf.org>
2015-04-17 16:30:39 -0400

    Carte du monde: ajouté les bureaux

--- project/cmsplugin_carte/urls.py
+++ project/cmsplugin_carte/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'project.cmsplugin_carte.views',
-    url(r'^pays.json', 'pays_json'),
+    url(r'^pays.json', 'pays_json', name='cmsplugin_carte-pays_json'),
+    url(r'^bureaux.json', 'bureaux_json', name='cmsplugin_carte-bureaux_json'),
 )
--- project/cmsplugin_carte/views.py
+++ project/cmsplugin_carte/views.py
@@ -2,9 +2,22 @@
 
 import json
 
-from auf.django.references.models import Pays
+from auf.django.references.models import Pays, Region
 from django.http import HttpResponse
 
+
+LAT_LONG = {
+    'A': (45.5, -73.55),
+    'ACGL': (3.867, 11.517),
+    'AO': (14.683, -17.433),
+    'AP': (21.033, 105.85),
+    'C': (18.533, -72.333),
+    'ECO': (44.433, 26.1),
+    'EO': (50.833, 4.35),
+    'M': (34.033, -6.833),
+    'MO': (33.883, 35.5),
+    'OI': (-18.933, 47.517),
+}
 
 def pays_json(request):
     data = {}
@@ -14,4 +27,19 @@ def pays_json(request):
             'name': pays.nom,
             'fillKey': 'B' + pays.region.code
         }
+    return HttpResponse(json.dumps(data), mimetype='application/json')
+
+
+def bureaux_json(request):
+    regions = Region.objects.all()
+    data = []
+    for region in regions:
+        if region.code in LAT_LONG:
+            latitude, longitude = LAT_LONG[region.code]
+            data.append({
+                'latitude': latitude,
+                'longitude': longitude,
+                'radius': 4,
+                'nom': 'Bureau ' + region.nom
+            })
     return HttpResponse(json.dumps(data), mimetype='application/json')
