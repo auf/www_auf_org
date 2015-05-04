088032f814e052235346f157eab4c370c1eb236d
Patrick Hetu <patrick.hetu@auf.org>
2015-05-04 14:27:44 -0400

    ckeditor: ajout du plugin fontawesome

--- project/djangocms_bureaux/static/djangocms_text_ckeditor/ckeditor/plugins/fontawesome/plugin.js
+++ project/djangocms_bureaux/static/djangocms_text_ckeditor/ckeditor/plugins/fontawesome/plugin.js
@@ -1,21 +1,26 @@
+/*
+    Author  : Michael Janea (https://facebook.com/mbjanea)
+    Version : 1.0
+*/
+
 CKEDITOR.plugins.add('fontawesome', {
     requires: 'widget',
-    icons: 'fontawesome',
-    init: function(editor){
+    icons   : 'fontawesome',
+    init    : function(editor){
         editor.widgets.add('FontAwesome', {
-            button: 'Insert Font Awesome',
-            template: '<span class="" style=""></span>',
-            dialog: 'fontawesomeDialog',
-            allowedContent: 'span(!fa){style}',
-            upcast: function(element){
+            button          : 'Insert Font Awesome',
+            template        : '<span class="" style=""></span>',
+            dialog          : 'fontawesomeDialog',
+            allowedContent  : 'span(!fa){style}',
+            upcast          : function(element){
                 return element.name == 'span' && element.hasClass('fa');
             },
-            init: function(){
+            init            : function(){
                 this.setData('class', this.element.getAttribute('class'));
                 this.setData('color', this.element.getStyle('color'));
                 this.setData('size', this.element.getStyle('font-size'));
             },
-            obj.data(obj{
+            data            : function(){
                 var istayl = '';
                 this.element.setAttribute('class', this.data.class);
                 istayl += this.data.color != '' ? 'color:' + this.data.color + ';' : '';
@@ -26,5 +31,6 @@ CKEDITOR.plugins.add('fontawesome', {
         });
         
         CKEDITOR.dialog.add('fontawesomeDialog', this.path + 'dialogs/fontawesome.js');
+        CKEDITOR.document.appendStyleSheet(CKEDITOR.plugins.getPath('fontawesome') + 'font-awesome/css/font-awesome.min.css');
     }
-});): function()
+});
