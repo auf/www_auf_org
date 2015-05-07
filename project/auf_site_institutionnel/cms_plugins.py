2b3f1f8dd5e5e6524f528ef4d705ebfa7314fa55
Patrick Hetu <patrick.hetu@auf.org>
2015-05-07 14:52:44 -0400

    Revert "cms: création d'un plugin pour lister les implantations"
    This reverts commit 1616517b3c55c3d0172bf8c2e1730d5896f63183.

--- project/auf_site_institutionnel/cms_plugins.py
+++ project/auf_site_institutionnel/cms_plugins.py
@@ -13,7 +13,6 @@ from project.auf_site_institutionnel.models import Partenaire
 #from newsletter.models import *
 from .models import EmployePlugin, ImplantationPlugin
 
-
 class CMSMembrePlugin(CMSPluginBase):
     name = _("Membre")
     render_template = "auf_site_institutionnel/membrePlugin.html"
@@ -29,7 +28,7 @@ class CMSMembrePlugin(CMSPluginBase):
                         'placeholder':placeholder})
         return context
 
-#plugin_pool.register_plugin(CMSMembrePlugin)
+plugin_pool.register_plugin(CMSMembrePlugin)
 
 
 class CMSImplantationPlugin(CMSPluginBase):
@@ -47,7 +46,7 @@ class CMSImplantationPlugin(CMSPluginBase):
                         'placeholder':placeholder})
         return context
 
-#plugin_pool.register_plugin(CMSImplantationPlugin)
+plugin_pool.register_plugin(CMSImplantationPlugin)
 
 
 class CMSPartenairePlugin(CMSPluginBase):
@@ -62,7 +61,7 @@ class CMSPartenairePlugin(CMSPluginBase):
                         'placeholder':placeholder})
         return context
 
-#plugin_pool.register_plugin(CMSPartenairePlugin)
+plugin_pool.register_plugin(CMSPartenairePlugin)
 
 
 class CMSEmployePlugin(CMSPluginBase):
@@ -84,7 +83,7 @@ class CMSEmployePlugin(CMSPluginBase):
         ctx['layout_template'] = instance.layout_template
         return ctx
 
-#plugin_pool.register_plugin(CMSEmployePlugin)
+plugin_pool.register_plugin(CMSEmployePlugin)
 
 
 class CMSImplantationPlugin(CMSPluginBase):
@@ -101,7 +100,7 @@ class CMSImplantationPlugin(CMSPluginBase):
         ctx['object_list'] = qs
         return ctx
 
-#plugin_pool.register_plugin(CMSImplantationPlugin)
+plugin_pool.register_plugin(CMSImplantationPlugin)
 
 #class CMSLettrePlugin(CMSPluginBase):
 #    name = _("Lettre")
