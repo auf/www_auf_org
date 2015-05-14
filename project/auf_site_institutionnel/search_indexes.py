e52be2b57ea8569bf51cbb40891be3126fd79d10
Patrick Hetu <patrick.hetu@auf.org>
2015-05-14 14:20:45 -0400

    recherche: amélioration de l'affichage

--- project/auf_site_institutionnel/search_indexes.py
+++ project/auf_site_institutionnel/search_indexes.py
@@ -93,6 +93,9 @@ class PublicationIndex(AufIndex, indexes.Indexable):
     def prepare_section(self, obj):
         return u"Publication"
 
+    def prepare_date_pub(self, obj):
+        return None
+
     def index_queryset(self, using=None):
         return Publication.objects.filter(status='3')
 
