8450562bcbd2613a47705e7391c695d5b0cd4e9c
Patrick Hetu <patrick.hetu@auf.org>
2015-05-14 15:18:34 -0400

    ne pas réinitialiser les dates de publication

--- project/auf_site_institutionnel/models.py
+++ project/auf_site_institutionnel/models.py
@@ -58,7 +58,7 @@ class Bourse(models.Model):
     image = models.ImageField(upload_to='bourse', null=True, blank=True)
     date_fin = models.DateField(null=True, blank=True)
     date_fin2 = models.CharField(max_length=1, null=True, blank=True, choices=(('1', '1 Moi avant le début des manifestations'), ('2', '2 Mois avant le début des manifestations'), ('3', '3 Mois avant le début des manifestations'), ('4', '4 Mois avant le début des manifestations'), ('5', '5 Mois avant le début des manifestations'), ('6', 'Permanent')))
-    date_pub = models.DateTimeField('date de creation', auto_now=True)
+    date_pub = models.DateTimeField('date de creation')
     date_mod = models.DateTimeField('date de derniere modification', auto_now_add=True)
     status = models.CharField(max_length=1, default='3', null=False, blank=False, choices=(('1', 'En cours de redaction'), ('2', 'Propose a la publication'), ('3', 'Publie en Ligne'), ('4', 'A supprimer')))
 
@@ -144,7 +144,7 @@ class Appel_Offre(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to='appel_offre')
     date_fin = models.DateField(null=True, blank=True)
     date_fin2 = models.CharField(max_length=1, null=True, blank=True, choices=(('1', '1 Moi avant le début des manifestations'), ('2', '2 Mois avant le début des manifestations'), ('3', '3 Mois avant le début des manifestations'), ('4', '4 Mois avant le début des manifestations'), ('5', '5 Mois avant le début des manifestations'), ('6', 'Permanent')))
-    date_pub = models.DateTimeField('date de creation', auto_now=True)
+    date_pub = models.DateTimeField('date de creation')
     date_mod = models.DateTimeField('date de derniere modification', auto_now_add=True)
     status = models.CharField(max_length=1, default='3', null=False, blank=False, choices=(('1', 'En cours de redaction'), ('2', 'Propose a la publication'), ('3', 'Publie en Ligne'), ('4', 'A supprimer')))
 
