393cd2b80008252272290e5c619d96b1b44fa074
Patrick Hetu <patrick.hetu@auf.org>
2015-05-29 15:25:30 -0400

    carte: correction du filtre des bureaux

--- project/cmsplugin_carte/cms_plugins.py
+++ project/cmsplugin_carte/cms_plugins.py
@@ -1,10 +1,17 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
+from auf.django.references.models import Region
+
 
 class CarteCMSPlugin(CMSPluginBase):
     name = "Carte du monde"
     render_template = "cmsplugin_carte/base.html"
     admin_preview = False
+
+    def render(self, context, instance, placeholder):
+        context.update({'bureaux': Region.objects.filter(actif=1)})
+        return context
+
 
 plugin_pool.register_plugin(CarteCMSPlugin)
