7b74c74fd3f9bce14b13ff8f78e34d67e7d6f418
benselme <benselme@gmail.com>
2015-10-30 17:07:02 -0400

    Première version migration vers blog (incomplète)

--- project/importa/management/commands/importa.py
+++ project/importa/management/commands/importa.py
@@ -1,96 +1,94 @@
 # encoding: utf-8
-import sys
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
 
-from django.core.management.base import BaseCommand, CommandError
-from django.core.files import File as DjangoFile
+from django.core.management.base import BaseCommand
+from django.template import RequestContext
+from django.test.client import RequestFactory
+import djangocms_blog.models as blog_models
+import filer.models as filer_models
+from django.db import transaction
 
-from cms import api
-from cms.models import Page
-
-from adminfiles.models import FileUpload
-from adminfiles.parse import UPLOAD_RE
-
-from filer.models.imagemodels import Image
-#from filer.models.filemodels import File
-from project.djangocms_bureaux.models import AufFile as File
-from filer.models.foldermodels import Folder
-
-from cmsplugin_filer_file.models import FilerFile
-from cmsplugin_filer_file.cms_plugins import FilerFilePlugin
+from project.auf_site_institutionnel.models import (
+     Bourse, Actualite, Veille, Appel_Offre, Evenement, Publication)
 
 
-from project.auf_site_institutionnel.models import Actualite,\
-    Bourse, Actualite, Veille, Appel_Offre, Evenement, Publication
+# http://stackoverflow.com/questions/26337120/django-cms-how-to-get-placeholder-html-content
+def render_placeholder_html(placeholder):
+    from cms import plugin_rendering
+    request_factory = RequestFactory()
+    request = request_factory.get('/')
+    request.session = {}
+    request.LANGUAGE_CODE = settings.LANGUAGE_CODE
 
-#from project.djangocms_bureaux.models import ImageExtension
-
-
-def _adminfile_to_filer(slug, ph, parent):
-    print(slug)
-    try:
-        upload = FileUpload.objects.get(slug=slug)
-        #print(upload.upload.file)
-    except FileUpload.DoesNotExist, e:
-        print(e)
-        upload = None
-        return ""
-
-    if upload.is_image():
-        try:
-            obj =Image(file=DjangoFile(open(upload.upload.path, 'rb')), name=upload.upload.name)
-            obj.save()
-        except:
-            print("wget %s" % upload.upload.path.replace('/srv/www/test-',''))
-            return ''#raise
-        filer_file = api.add_plugin(ph, "FilerImagePlugin", "fr", target=parent, image=obj, alt_text=upload.title, width=380,height=285)
-    else:
-        try:
-            obj =File(file=DjangoFile(open(upload.upload.path, 'rb')), name=upload.upload.name)
-            obj.save()
-        except:
-            print("wget %s" % upload.upload.path.replace('/srv/www/test-',''))
-            return ''#raise
-        filer_file = api.add_plugin(ph, "FilerFilePlugin", "fr", target=parent, file=obj, title=upload.title)
-
-    folder = Folder.objects.all()[1]
-    obj.folder = folder
-    obj.save()
-
-    return """<img src="/static/filer/icons/file_32x32.png" alt="%s" title="%s" id="plugin_obj_%s">""" % (upload.title, upload.title, filer_file.id)
+    # Needed for plugin rendering.
+    request.current_page = None
+    request.user = AnonymousUser()
+    return plugin_rendering.render_placeholder(placeholder,
+                                               RequestContext(request))
 
 
 class Command(BaseCommand):
 
+    @transaction.commit_on_success
     def handle(self, *args, **options):
-        #page_actualite = api.create_page(u"Actualité", "trois_colonnes.html", "fr")
-        #for i in [Bourse, Actualite, Veille, Appel_Offre, Evenement, Publication]:
-        for i in [Actualite, Bourse, Veille, Appel_Offre, Evenement, Publication]:
-            for a in i.objects.all():
-                #print(a.pk)
-                #page = api.create_page(a.titre, "trois_colonnes.html", "fr", slug=a.slug,
-                #                       meta_description=a.resume, publication_date=a.date_pub,
-                #                       published=True, parent=page_actualite)
-                #if a.image:
-                #    obj = File(open(a.image.path, 'rb'), name=a.image.name)
-                #    image = Image.objects.create(file=obj, original_filename=a.image.name)
-                #    ImageExtension.objects.create(extended_object=page, image=image)
+        sp = transaction.savepoint()
+        try:
+            actualites = blog_models.BlogCategory.objects.create(name=u"Actualités",
+                                                                 slug="actualites")
+            veilles = blog_models.BlogCategory.objects.create(name=u"Veilles",
+                                                              slug="veille")
+            appels_offres = blog_models.BlogCategory.objects.create(
+                name=u"Appels d'offres", slug="appels_offres")
+            evenements = blog_models.BlogCategory.objects.create(name=u"Évènements",
+                                                                 slug="evenements")
+            publications = blog_models.BlogCategory.objects.create(
+                name=u"Publications", slug="publication")
+            bourse = blog_models.BlogCategory.objects.create(name=u"Bourse",
+                                                             slug="bourse")
 
-                #ph = page.placeholders.get(slot="texte")
+            categories = {
+                Actualite: actualites,
+                Bourse: bourse,
+                Veille: veilles,
+                Appel_Offre: appels_offres,
+                Evenement: evenements,
+                Publication: publications
+            }
+            old_models = categories.keys()
 
-		try:
-	                a.save()
-		except:
-			continue
-                print(a.id)
-                if a.cmstexte:
-                    try:
-                        a.cmstexte.cmsplugin_set.all().delete()
-                    except:
-                        print(sys.exc_info()[0])
-                texte = a.texte
-                txt = api.add_plugin(a.cmstexte, "TextPlugin", "fr")
-                for match in UPLOAD_RE.finditer(texte):
-                    img = _adminfile_to_filer(match.group(1), a.cmstexte, txt)
-                    texte = UPLOAD_RE.sub(img, texte, 1)
-                txt.body=texte
-                txt.save()
+            for i in old_models:
+                for a in i.objects.all():
+                    print(a.id)
+                    if hasattr(a, 'date_debut'):
+                        date_debut = a.date_debut
+                    else:
+                        date_debut = a.date_pub
+                    # noinspection PyArgumentList
+                    post = blog_models.Post(
+                        date_created=a.date_debut,
+                        date_modified=a.date_mod,
+                        date_published=a.date_pub,
+                        date_published_end=a.date_fin,
+                        title=a.titre,
+                        slug=a.slug,
+                        abstract=a.resume,
+                        post_text=render_placeholder_html(a.cmstexte)
+                    )
+
+                    image = filer_models.Image.objects.get_or_create(
+                        file=a.image.file, defaults={
+                            'name': a.slug, 'description': a.titre
+                        })
+
+                    post.main_image = image
+                    post.categories.add()
+                    tags = []
+                    if a.status in ('5', '3'):
+                        tags.append('International')
+                    if a.status in ('6', '3'):
+                        for region in a.bureau_set.all():
+                            tags.append(region.nom)
+                    post.save()
+        finally:
+            transaction.savepoint_rollback(sp)
